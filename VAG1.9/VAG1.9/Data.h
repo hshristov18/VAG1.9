7fe3279d1461f113cde9a62011858485d1923a4b
Ivan Dimov <iidimov18@codingburgas.bg>
2021-03-04 10:21:56 +0200

    Add data logic from main branch

---
+++ VAG1.9/VAG1.9/Data.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include "Struct.h"
+#include <stdlib.h>
+using namespace std;
+STUDENT generateStudent();
+
+string randomFirstName();
+
+string randomSurname();
+
+string randomClass();
+
+string randomRole();
+
+string randomEmail();
---
+++ VAG1.9/VAG1.9/VAG1.9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Data.h"
+using namespace std;
+int main()
+{
+	cout << randomFirstName() << endl;
+	cout << randomSurname() << endl;
+	cout << randomClass() << endl;
+	cout << randomRole() << endl;
+	cout << randomEmail() << endl;
+}
